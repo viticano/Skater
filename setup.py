f833b2ddd77d4bb5a858c58cd431eb713c08561b
aikramer2 <aikramer2@gmail.com>
2017-01-26 20:41:07 -0800

    setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='PyInterpret',
+      version='0.0.1',
+      description='Model Agnostic Interpretation Library',
+      author='Aaron Kramer',
+      author_email='aaron@datascience.com',
+      url='https://github.com/datascienceinc/PyInterpret/tree/master',
+      packages=['PyInterpret'],
+      install_requires = ['numpy', 'scipy'],
+      include_package_data=True,
+      zip_safe=False      
+     )
