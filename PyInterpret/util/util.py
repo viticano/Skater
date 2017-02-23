9a618a810c500196867178f4a786220d82ad7e57
aikramer2 <aikramer2@gmail.com>
2017-02-23 14:49:17 -0800

    file renames

--- PyInterpret/util/util.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def get_uniform_ball_around_point(x, n=100, fixed_scale = 1.0):
-    fixed_scale = fixed_scale or 1.0
-
-    return np.random.normal(x, np.ones(*x.shape) * fixed_scale, size=(n, x.shape[0]))
-
-def get_scaled_ball_around_point(x, n=100):
-    pass 
