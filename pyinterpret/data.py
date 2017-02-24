6dc8667b0a2fe8cf52f7b7ccbbd86b123c96f7d2
aikramer2 <aikramer2@gmail.com>
2017-02-24 14:42:54 -0600

    comments, exception handling, checks

--- pyinterpret/data.py
+++ pyinterpret/data.py
@@ -4,8 +4,18 @@ from scipy import stats
 import pandas as pd
 
 class DataSet(object):
-	def __init__(self, data, feature_names = None, index = None, n_percentiles = 100):
+	def __init__(self, data, feature_names = None, index = None):
+		"""
+		The abtraction around using, accessing, sampling data for interpretation purposes.
 
+		Parameters
+		----------
+			data: 1D or 2D numpy array.
+			feature_names: iterable of feature names
+			index: iterable of row names
+
+
+		"""
 		assert isinstance(data, np.ndarray), 'Data needs to be a numpy array'
 
 		ndim = len(data.shape) 
@@ -30,7 +40,39 @@ class DataSet(object):
 
 
 	def generate_grid(self, feature_ids, grid_resolution = None, grid_range = None):
-		assert all(i >= 0 and i <= 1 for i in grid_range), "Grid range values must be between 0 and 1"
+		"""
+		Generates a grid of values on which to compute pdp. For each feature xi, for value
+		yj of xi, we will fix xi = yj for every observation in X.
+
+		Parameters
+		----------
+			feature_ids(list): 
+				Feature names for which we'll generate a grid. Must be contained
+				by self.feature_ids
+
+			grid_resolution(int):
+				The number of unique values to choose for each feature.
+
+			grid_range(tuple):
+				The percentile bounds of the grid. For instance, (.05, .95) corresponds to
+				the 5th and 95th percentiles, respectively.
+
+		Returns
+		----------
+		grid(numpy.ndarray): 	There are as many rows as there are feature_ids
+								There are as many columns as specified by grid_resolution
+		"""
+
+
+		grid_range_warning = "Grid range values must be between 0 and 1"
+		assert all(i >= 0 and i <= 1 for i in grid_range), grid_range_warning
+
+		grid_resolution_warning = "Grid resolute must be a positive integer"
+		assert isinstance(grid_resolution, int) and grid_resolution > 0, grid_resolution_warning
+
+		feature_id_warning = "Must pass in feature ids contained in DataSet.feature_ids"
+		assert all(feature_id in self.feature_ids for feature_id in feature_id), feature_id_warning
+
 
 		grid_range = map(lambda x: x* 100, grid_range)
 		bins = np.linspace(*grid_range, num=grid_resolution)
@@ -42,11 +84,7 @@ class DataSet(object):
 		
 
 
-	def build_metastore(self):
-		#self.bins = (np.array(range(self.n_percentiles)) / float(self.n_percentiles)) * 100
-		#self.percentiles = np.array(map(lambda i: np.percentile(self.data, i, axis=0), self.bins))
-		#last = (self.percentiles[-1] - self.percentiles[-2]).reshape(1, self.dim)
-		#self._augment = np.concatenate((self.percentiles.copy(), last))[1:]
+	def _build_metastore(self):
 		medians = np.median(self.data.values, axis = 0).reshape(1, self.dim)
 		dists = cosine_distances(self.data.values, Y = medians).reshape(-1)
 		dist_percentiles = map(lambda i: int(stats.percentileofscore(dists, i)), dists)
@@ -69,9 +107,30 @@ class DataSet(object):
 	def __setitem__(self, key, newval):
 		self.data.__setitem__(key, newval)
 		
-	def generate_sample(self, sample = True, n_samples_from_dataset = 1000, strategy = 'random-choice', replace = True, samples_per_bin = 20):
+	def generate_sample(self, sample = True, strategy = 'random-choice', n_samples_from_dataset = 1000, 
+						replace = True, samples_per_bin = 20):
+		'''
+		Method for generating data from the dataset.
 
-		#
+		Parameters:
+		-----------
+			sample(Bool): 
+				If False, we'll take the full dataset, otherwise we'll sample.
+
+			n_samples_from_dataset(int): 
+				Specifies the number of samples to return. Only implemented 
+				if strategy is "random-choice".
+
+			replace(Bool):
+				Bool for sampling with or without replacement
+
+			samples_per_bin(int):
+				If strategy is uniform-over-similarity-ranks, then this is the number
+				of samples to take from each discrete rank.
+
+
+		'''
+		
 
 		if not sample:
 			return self.data
@@ -88,11 +147,10 @@ class DataSet(object):
 		elif strategy == 'uniform-over-similarity-ranks':
 			
 			if not self.metastore:
-				self.metastore = self.build_metastore()
+				self.metastore = self._build_metastore()
 
 			data_distance_ranks = self.metastore['ranks_rounded']
-	
-			
+
 			for i in range(100):
 				j = i / 100.
 				idx = np.where(data_distance_ranks==j)[0]
