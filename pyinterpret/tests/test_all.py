e6fa38bad9cec8998089bed21293c9ba2f3ba2bd
aikramer2 <aikramer2@gmail.com>
2017-03-01 19:22:10 +0000

    more tests for DSTM-407

--- pyinterpret/tests/test_all.py
+++ pyinterpret/tests/test_all.py
@@ -1,9 +1,11 @@
 import unittest
 from test_data import TestData
 from test_lime import TestLime
+from test_partial_dependence import TestPartialDependence
 
+pdp_tests = unittest.TestLoader().loadTestsFromTestCase(TestPartialDependence)
 dataset_tests = unittest.TestLoader().loadTestsFromTestCase(TestData)
 lime_tests = unittest.TestLoader().loadTestsFromTestCase(TestLime)
 
-for test in [dataset_tests, lime_tests]:
+for test in [dataset_tests, lime_tests, pdp_tests]:
     unittest.TextTestRunner(verbosity=2).run(test)
