b11853cb1b8a619b065fa9257db411890d2e0905
aikramer2 <aikramer2@gmail.com>
2017-03-13 09:36:33 -0700

    restructured model to have separate files for deploy and in-memory models

---
+++ pyinterpret/model/local.py
@@ -0,0 +1,25 @@
+from .model import Model
+
+class InMemoryModel(Model):
+    """This model can be called directly from memory"""
+    def __init__(self, prediction_fn, examples=None):
+        """This model can be called directly from memory
+        Parameters
+        ----------
+        prediction_fn(callable)
+            function that returns predictions
+
+        examples(numpy.array or pandas.dataframe):
+            examples to use to make inferences about the function.
+            prediction_fn must be able to take examples as an
+            argument.
+        """
+        super(InMemoryModel, self).__init__(self)
+        self.prediction_fn = prediction_fn
+        self.examples = np.array(examples)
+        # if self.examples.any():
+        #     self.check_output_signature(self.examples)
+
+    def predict(self, *args, **kwargs):
+        """Just use the function itself for predictions"""
+        return self.formatter(self.prediction_fn(*args, **kwargs))
---
+++ pyinterpret/model/remote.py
@@ -0,0 +1,41 @@
+from .model import Model
+
+class DeployModel(Model):
+    """Model that gets predictions from a deployed model"""
+    def __init__(self, uri, parse_function=None):
+        """This model can be called by making http requests
+        to the passed in uri.
+
+        Parameters
+        ----------
+        uri(string)
+            Where to post requests
+
+        parse_function(callable)
+            This function will run on outputs before returning
+            results to interpretation objects.
+        """
+        super(WebModel, self).__init__(self)
+        self.uri = uri
+        self.parse_function = parse_function or self.default_parser
+
+    @staticmethod
+    def default_parser(content):
+        """Just returns raw results"""
+        return content
+
+    def predict(self, request_body, **kwargs):
+        if self.__confirm_server_is_healthy(self.uri):
+            output = self.parse_function(requests.get(self.uri, data=request_body, **kwargs).content)
+            return output
+        else:
+            raise ValueError("Server is not running.")
+
+    def __call__(self, *args, **kwargs):
+        """Just use the function itself for predictions"""
+        return self.predict(*args, **kwargs)
+
+    @staticmethod
+    def __confirm_server_is_healthy(uri):
+        """Makes sure uri is up"""
+        return bool(uri)
