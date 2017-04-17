cf3ba82a9317cd73312734035b4a7e93742b7c20
aikramer2 <aikramer2@gmail.com>
2017-04-17 13:01:03 -0700

    fixed import issue and updated travis

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='pyinterpret',
       packages=find_packages(),
       install_requires=['scikit-learn>=0.18', 'pandas>=0.19', 'lime>=0.1.1.20', 'requests',
                         'pathos==0.2.0', 'dill>=0.2.6'],
+      extras_require ={'all':'matplotlib'},
       include_package_data=True,
       zip_safe=False,
       dependency_links=['https://github.com/datascienceinc/lime/tarball/v1-release#egg=lime-0.1.1.20'])
