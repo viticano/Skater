figsize as kwarg, and fixed bug when gradient map box was blank when partial dependence of one of the features has 0 effect

lynxes/core/global_interpretation/partial_dependence.py
@@ -338,7 +338,7 @@ class PartialDependence(BaseGlobalInterpretation):
                                 grid_resolution=None, grid_range=None, n_jobs=-1,
                                 sample=True, sampling_strategy='random-choice',
                                 n_samples=10000, bin_count=50, samples_per_bin=10,
-                                with_variance=False):
+                                with_variance=False, figsize=(16,10)):
         """
         Computes partial_dependence of a set of variables. Essentially approximates
         the partial partial_dependence of the predict_fn with respect to the variables
@@ -455,7 +455,7 @@ class PartialDependence(BaseGlobalInterpretation):
                                                       n_jobs=n_jobs, return_metadata=True)
 
             self.interpreter.logger.info("done computing pd, now plotting ...")
-            ax = self._plot_pdp_from_df(pd_df, metadata, with_variance=with_variance)
+            ax = self._plot_pdp_from_df(pd_df, metadata, with_variance=with_variance, figsize=figsize)
             return ax
         else:
             ax_list = []
@@ -470,14 +470,14 @@ class PartialDependence(BaseGlobalInterpretation):
                                                           n_jobs=n_jobs, return_metadata=True)
 
                 self.interpreter.logger.info("done computing pd, now plotting ...")
-                ax = self._plot_pdp_from_df(pd_df, metadata, with_variance=with_variance)
+                ax = self._plot_pdp_from_df(pd_df, metadata, with_variance=with_variance, figsize=figsize)
                 ax_list.append(ax)
             return ax_list
 
 
     def _plot_pdp_from_df(self, pdp, pd_metadata,
                           with_variance=False, plot_title=None,
-                          disable_offset=True):
+                          disable_offset=True, figsize=(16,10)):
 
         feature_columns = pd_metadata['feature_columns']
         class_columns = pd_metadata['class_columns']
@@ -485,14 +485,24 @@ class PartialDependence(BaseGlobalInterpretation):
         n_features = len(feature_columns)
         if n_features == 1 or not hasattr(feature_columns, "__iter__"):
             feature_column = feature_columns[0]
-            return self._2d_pdp_plot(pdp, feature_column, sd_col, class_columns,
+            return self._2d_pdp_plot(pdp,
+                                     feature_column,
+                                     sd_col,
+                                     class_columns,
                                      with_variance=with_variance,
-                                     plot_title=plot_title, disable_offset=disable_offset)
+                                     plot_title=plot_title,
+                                     disable_offset=disable_offset,
+                                     figsize=figsize)
         elif n_features == 2:
             feature1_column, feature2_column = feature_columns
-            return self._3d_pdp_plot(pdp, feature1_column, feature2_column,
-                                     sd_col, class_columns, with_variance=with_variance,
-                                     plot_title=plot_title)
+            return self._3d_pdp_plot(pdp,
+                                     feature1_column,
+                                     feature2_column,
+                                     sd_col,
+                                     class_columns,
+                                     with_variance=with_variance,
+                                     plot_title=plot_title,
+                                     figsize=figsize)
         else:
             msg = "Something went wrong. Expected either a single feature, " \
                   "or a 1-2 element array of features, got array of size:" \
@@ -501,7 +511,8 @@ class PartialDependence(BaseGlobalInterpretation):
 
 
     def _2d_pdp_plot(self, pdp, feature_name, sd_col, class_columns,
-                     with_variance=False, plot_title=None, disable_offset=True):
+                     with_variance=False, plot_title=None,
+                     disable_offset=True, figsize=(16,10)):
         colors = cycle(COLORS)
         figure_list, axis_list = [], []
 
@@ -512,7 +523,7 @@ class PartialDependence(BaseGlobalInterpretation):
         for class_column in class_columns:
             # if class_name is None:
             #     raise ValueError("Could not parse class name from {}".format(mean_col))
-            f, ax = pyplot.subplots(1)
+            f, ax = pyplot.subplots(1, figsize=figsize)
             figure_list.append(f)
             axis_list.append(ax)
             color = next(colors)
@@ -558,8 +569,8 @@ class PartialDependence(BaseGlobalInterpretation):
 
 
     def _3d_pdp_plot(self, pdp, feature1, feature2, sd_column, class_columns,
-
-                     with_variance=False, plot_title=None, disable_offset=True):
+                     with_variance=False, plot_title=None, disable_offset=True,
+                     figsize=(16, 10)):
 
         # if there are just 2 classes, pick the last one.
         if len(class_columns) == 2:
@@ -572,16 +583,22 @@ class PartialDependence(BaseGlobalInterpretation):
         feature_2_is_binary = len(np.unique(feature_2_data)) == 2
 
         if not feature_1_is_binary and not feature_2_is_binary:
-            plot_objects = self._plot_3d_full_mesh(pdp, feature1, feature2,
-                                                   sd_column, class_columns,
-                                                   with_variance=with_variance)
+            plot_objects = self._plot_3d_full_mesh(pdp,
+                                                   feature1,
+                                                   feature2,
+                                                   sd_column,
+                                                   class_columns,
+                                                   with_variance=with_variance,
+                                                   figsize=figsize)
 
         elif feature_1_is_binary and feature_2_is_binary:
             plot_objects = self._plot_2d_2_binary_feature(pdp,
-                                                   feature1,
-                                                   feature2,
-                                                   sd_column, class_columns,
-                                                   with_variance=with_variance)
+                                                          feature1,
+                                                          feature2,
+                                                          sd_column,
+                                                          class_columns,
+                                                          with_variance=with_variance,
+                                                          figsize=figsize)
         else:
             # one feature is binary and one isnt.
             binary_feature, non_binary_feature = {
@@ -608,7 +625,7 @@ class PartialDependence(BaseGlobalInterpretation):
 
     def _plot_3d_full_mesh(self, pdp, feature1, feature2,
                            sd_column, class_columns,
-                           with_variance=False, alpha=.7):
+                           with_variance=False, alpha=.7, figsize=(16,10)):
         colors = cycle(COLORS)
 
         figure_list, axis_list = [], []
@@ -616,9 +633,9 @@ class PartialDependence(BaseGlobalInterpretation):
         for class_column in class_columns:
             gradient_x, gradient_y, X, Y, Z = self.compute_3d_gradients(pdp, class_column, feature1, feature2)
             color_gradient, xmin, xmax, ymin, ymax = coordinate_gradients_to_1d_colorscale(gradient_x, gradient_y)
-            pyplot.figure()
+            figure=pyplot.figure(figsize=figsize)
             ax = pyplot.subplot2grid((3, 3), (0, 0), colspan=2, rowspan=3, projection='3d')
-            figure_list.append(ax.figure)
+            figure_list.append(figure)
             axis_list.append(ax)
 
             surface = ax.plot_surface(X, Y, Z, alpha=alpha, facecolors=color_gradient, linewidth=0., rstride=1, cstride=1)
@@ -653,11 +670,11 @@ class PartialDependence(BaseGlobalInterpretation):
 
 
     def _plot_3d_2_binary_feature(self, pdp, feature1, feature2, sd_column,
-                                  class_columns, with_variance=False):
+                                  class_columns, with_variance=False, figsize=(16, 10)):
         colors = cycle(COLORS)
         figure_list, axis_list = [], []
         for class_column in class_columns:
-            fig = pyplot.figure()
+            fig = pyplot.figure(figsize=figsize)
             ax = fig.add_subplot(111, projection='3d')
 
             for val in np.unique(pdp[feature2]):
@@ -678,12 +695,13 @@ class PartialDependence(BaseGlobalInterpretation):
         return flatten([figure_list, axis_list])
 
     def _plot_2d_2_binary_feature(self, pdp, feature1, feature2, sd_col,
-                                  class_columns, with_variance=False):
+                                  class_columns, with_variance=False,
+                                  figsize=(16,10)):
         figure_list, axis_list = [], []
 
         std_error = pdp.set_index([feature1, feature2])[sd_col].unstack()
         for class_column in class_columns:
-            f = pyplot.figure()
+            f = pyplot.figure(figsize=figsize)
             ax = f.add_subplot(111)
             #feature2 is columns
             #feature1 is index
@@ -710,14 +728,15 @@ class PartialDependence(BaseGlobalInterpretation):
 
     def _plot_2d_1_binary_feature_and_1_continuous(self, pdp, binary_feature,
                                                    non_binary_feature, sd_column,
-                                                   class_columns, with_variance=False):
+                                                   class_columns, with_variance=False,
+                                                   figsize=(16,10)):
 
         figure_list, axis_list = [], []
 
         binary_vals = np.unique(pdp[binary_feature])
         for class_column in class_columns:
             colors = cycle(COLORS)
-            f = pyplot.figure()
+            f = pyplot.figure(figsize=figsize)
             ax = f.add_subplot(111)
             figure_list.append(f)
             axis_list.append(ax)

lynxes/util/plotting.py
@@ -114,7 +114,7 @@ def coordinate_gradients_to_1d_colorscale(dx, dy,
     return color, xmin+xbuffer, xmax-xbuffer, ymin+ybuffer, ymax-ybuffer
 
 def plot_2d_color_scale(x1_min, x1_max, x2_min, x2_max, plot_point=None,
-                        resolution=10, ax=None):
+                        resolution=10, ax=None, x_buffer_prop=.1, y_buffer_prop=.1):
     """
     Return a generic plot of a 2D color scale
 
@@ -146,15 +146,18 @@ def plot_2d_color_scale(x1_min, x1_max, x2_min, x2_max, plot_point=None,
         raise (MatplotlibUnavailableError("Matplotlib is required but unavailable on your system."))
     except RuntimeError:
         raise (MatplotlibDisplayError("Matplotlib unable to open display"))
-
+    x1_min, x1_max, x1_buffer = build_buffer2(x1_min, x1_max)
+    x2_min, x2_max, x2_buffer = build_buffer2(x2_min, x2_max)
     ax.set_xlim(x1_min, x1_max)
     ax.set_ylim(x2_min, x2_max)
     x1 = np.linspace(x1_min, x1_max, resolution+1)
     x2 = np.linspace(x2_min, x2_max, resolution+1)
     x1_diff = x1[1] - x1[0]
     x2_diff = x2[1] - x2[0]
+    print("x1 diff: {}".format(x1_diff))
+    print("x2 diff: {}".format(x2_diff))
     x1, x2 = np.meshgrid(x1, x2)
-    colors_for_scale, a, b, c, d = coordinate_gradients_to_1d_colorscale(x1, x2)
+    colors_for_scale, a, b, c, d = coordinate_gradients_to_1d_colorscale(x1, x2, x_buffer_prop=0, y_buffer_prop=0)
 
     if ax is None:
         f, ax = pyplot.subplots(1)
@@ -175,7 +178,21 @@ def plot_2d_color_scale(x1_min, x1_max, x2_min, x2_max, plot_point=None,
 
 def build_buffer(x, buffer_prop=.1):
     xmin, xmax = min(0, np.percentile(x, 3)), max(np.percentile(x, 97), 0)
-    buffer = (xmax - xmin) * buffer_prop / 2.
+    if xmin != xmax:
+        buffer = (xmax - xmin) * buffer_prop / 2.
+    #all values are the same
+    else:
+        buffer = 0.01
+    xmin, xmax = xmin - buffer, xmax + buffer
+    return xmin, xmax, buffer
+
+
+def build_buffer2(xmin, xmax, buffer_prop=.1):
+    if xmin != xmax:
+        buffer = (xmax - xmin) * buffer_prop / 2.
+    #all values are the same
+    else:
+        buffer = 0.01
     xmin, xmax = xmin - buffer, xmax + buffer
     return xmin, xmax, buffer
 