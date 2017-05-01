0b2e48a46d418bcbf0e810fb8326d792ef8f6621
aikramer2 <aikramer2@gmail.com>
2017-05-01 22:27:49 +0000

    about config

---
+++ lynxes/about.py
@@ -0,0 +1,12 @@
+# Reference: http://python-packaging.readthedocs.io/en/latest/dependencies.html
+from setuptools import setup, find_packages
+
+
+__title__ = 'lynxes'
+__version__ = '1.0.0b'
+__summary__ = 'Model Agnostic Interpretation Library'
+__uri__ = 'https://github.com/datascienceinc/lynxes/'
+__author__ = 'Aaron Kramer, Pramit Choudhary'
+__email__ = 'aaron@datascience.com, pramit@datascience.com'
+__license__ = 'MIT'
+__docs__ = 'https://datascienceinc.github.io/model-interpretation/'
