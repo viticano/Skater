957d9e7e1a5630ab65e75f74080922f165c34146
Pramit Choudhary <pramitchoudhary@ip-172-30-0-3.us-west-2.compute.internal>
2017-05-11 02:21:24 -0700

    DSTM-628 minor renaming of deploy_model to deployed_model

---
+++ skater/model/deployed_model.py
@@ -0,0 +1,120 @@
+"Subclass of Model for deployed models"
+import requests
+import numpy as np
+from functools import partial
+
+from .base import ModelType
+
+
+class DeployedModel(ModelType):
+    """Model that gets predictions from a deployed model"""
+    def __init__(self, uri, input_formatter, output_formatter,
+                 log_level=30, target_names=None, examples=None, feature_names=None,
+                 request_kwargs={}):
+        """This model can be called by making http requests
+        to the passed in uri.
+
+        Parameters
+        ----------
+        uri: string
+            Where to post requests
+
+        input_formatter: callable
+            This function will run on input data before passing
+            to requests library. This usually should take array types
+            and convert them to JSON.
+
+        output_formatter: callable
+            This function will run on outputs before returning
+            results to interpretation objects. This usually should take
+            request objects and convert them to array types.
+
+        log_level: int
+            see skater.model.Model for details
+
+        target_names: array type
+            see skater.model.Model for details
+
+        examples:
+            see skater.model.Model for details
+        """
+        self.uri = uri
+        self.input_formatter = input_formatter
+        self.output_formatter = output_formatter
+        self.request_kwargs = request_kwargs
+        super(DeployedModel, self).__init__(examples=examples,
+                                            target_names=target_names,
+                                            input_formatter=input_formatter,
+                                            output_formatter=output_formatter,
+                                            log_level=log_level,
+                                            feature_names=feature_names)
+
+
+    def _execute(self, data):
+        """
+        Just use the function itself for predictions
+        """
+        return requests.post(self.uri, json=data, **self.request_kwargs)
+
+
+    @staticmethod
+    def default_input_wrapper(data, key='input'):
+        return {key: data.tolist()}
+
+
+    @staticmethod
+    def default_output_wrapper(response, key='prediction'):
+        return np.array(response.json()[key])
+
+
+    @staticmethod
+    def _predict(data, uri, input_formatter, output_formatter, request_kwargs={}, transformer=None):
+        """Static prediction function for multiprocessing usecases
+
+        Parameters
+        ----------
+        data: arraytype
+
+        uri: string
+            Where to post requests
+
+        input_formatter: callable
+            This function will run on input data before passing
+            to requests library. This usually should take array types
+            and convert them to JSON.
+
+        output_formatter: callable
+            This function will run on outputs before returning
+            results to interpretation objects. This usually should take
+            request objects and convert them to array types.
+
+        formatter: callable
+            function responsible for formatting model outputs as necessary. For instance,
+            one hot encoding multiclass outputs.
+
+        predict_fn: callable
+
+        Returns
+        -----------
+        predictions: arraytype
+        """
+        query = input_formatter(data)
+        response = requests.post(uri, json=query, **request_kwargs)
+        results = output_formatter(response)
+        if transformer:
+            results = transformer(results)
+        return results
+
+
+    def _get_static_predictor(self):
+
+        uri = self.uri
+        input_formatter = self.input_formatter
+        output_formatter = self.output_formatter
+        transformer = self.transformer
+        predict_fn = partial(self._predict,
+                             uri=uri,
+                             input_formatter=input_formatter,
+                             output_formatter=output_formatter,
+                             transformer=transformer)
+        return predict_fn
