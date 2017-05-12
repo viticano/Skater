c18a27edf2e327a35cd2d70da43c75e971ed7e35
aikramer2 <aikramer2@gmail.com>
2017-05-12 11:30:08 -0500

    flake on test

--- skater/tests/all_tests.py
+++ skater/tests/all_tests.py
@@ -3,11 +3,13 @@
 import unittest
 import sys
 
+
 def run_tests():
     """Run all tests in current directory with signature test*.py"""
     testsuite = unittest.TestLoader().discover('', pattern="test*.py")
     run = unittest.TextTestRunner(verbosity=2).run(testsuite)
     return run.wasSuccessful()
+
 
 if __name__ == '__main__':
     successful = run_tests()
