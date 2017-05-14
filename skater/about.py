bba007928260e89d144f9354ad3317b29e278c33
Pramit Choudhary <pramitchoudhary@ip-172-30-0-3.us-west-2.compute.internal>
2017-05-14 13:16:44 -0700

    DSTM-628 bumped up the minor version

--- skater/about.py
+++ skater/about.py
@@ -2,10 +2,10 @@
 # meant to define package metadata in one and only one place
 
 __title__ = 'skater'
-__version__ = '1.0.0b.1'
+__version__ = '1.0.0b.2'
 __summary__ = 'Model Agnostic Interpretation Library'
 __uri__ = 'https://github.com/datascienceinc/skater/'
-__author__ = 'Aaron Kramer, Pramit Choudhary, DataScience.com Team'
+__author__ = 'Aaron Kramer, DataScience.com Team, Pramit Choudhary'
 __email__ = 'dstm@datascience.com'
 __license__ = 'MIT'
 __docs__ = 'https://datascienceinc.github.io/Skater/'
