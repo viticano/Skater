e2823308641309e7fecd51aa3785118bcc26828e
aikramer2 <aikramer2@gmail.com>
2017-05-17 13:37:58 -0700

    placeholder distribution

--- skater/about.py
+++ skater/about.py
@@ -2,7 +2,7 @@
 # meant to define package metadata in one and only one place
 
 __title__ = 'skater'
-__version__ = '1.0.0b.2'
+__version__ = '1.0.0'
 __summary__ = 'Coming Soon'
 __uri__ = 'https://github.com/datascienceinc/skater/'
 __author__ = 'Aaron Kramer, DataScience.com Team, Pramit Choudhary'
