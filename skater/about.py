903ea34792fbffac47e01d0337ae85814d5d8277
aikramer2 <aikramer2@gmail.com>
2017-05-17 13:10:33 -0700

    update

---
+++ skater/about.py
@@ -0,0 +1,11 @@
+# Reference: http://python-packaging.readthedocs.io/en/latest/dependencies.htmlfrom setuptools import setup, find_packages
+# meant to define package metadata in one and only one place
+
+__title__ = 'skater'
+__version__ = '1.0.0b.2'
+__summary__ = 'Coming Soon'
+__uri__ = 'https://github.com/datascienceinc/skater/'
+__author__ = 'Aaron Kramer, DataScience.com Team, Pramit Choudhary'
+__email__ = 'dstm@datascience.com'
+__license__ = 'MIT'
+__docs__ = 'Coming Soon'
